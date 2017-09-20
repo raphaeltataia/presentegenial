405bd16aedb3b719365dd28d046812d5b7546325
Raphael Tataia <raphaeltataia@Tataias-Macbook-Pro.local>
2017-09-20 03:52:41 -0300

    changing models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
 import datetime
 
 # Create your models here.
@@ -10,7 +9,7 @@ import datetime
 
 class Campaign(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey('Profile')
+    user = models.ForeignKey('app.User', related_name='campaigns', on_delete=models.CASCADE)
     product = models.ForeignKey('Product')
     tag = models.ManyToManyField('Tag')
     title = models.CharField(max_length=200)
@@ -25,31 +24,17 @@ class Campaign(models.Model):
         return self.title
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class User(AbstractUser):
     location = models.CharField(max_length=30, blank=True)
     name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=50)
-
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
-
-    def __str__(self):
-        return self.name
+    birthday = models.DateField(null=True, blank=True)
 
 
 class Product(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     description = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
@@ -59,7 +44,7 @@ class Product(models.Model):
 class Donation(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
     campaign = models.ForeignKey('Campaign')
-    user = models.ForeignKey('Profile')
+    user = models.ForeignKey('app.User', related_name='donations', on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
@@ -70,7 +55,12 @@ class Donation(models.Model):
 class Tag(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
-    slug = models.CharField(max_length=30)
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+
+        super(Tag, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.price
+        return self.name
