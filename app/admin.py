fcedf4a507c401222aaf0f079e5c0d8e3ba693f9
Raphael Tataia <raphaeltataia@Tataias-Macbook-Pro.local>
2017-09-20 03:54:30 -0300

    finishing admin models

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Campaign
-from .models import Profile
+from .models import User
 from .models import Product
 from .models import Donation
 from .models import Tag
@@ -9,27 +9,27 @@ from .models import Tag
 
 
 class CampaignAdmin(admin.ModelAdmin):
-    list_display = 'auto_increment_id', 'user', 'product', 'tag', 'title', 'description', 'created_date', 'end_date'
+    list_display = ('auto_increment_id', 'user', 'product', 'tag', 'title', 'description', 'created_date', 'end_date')
 
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = 'user', 'location', 'name', 'email'
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('location', 'full_name', 'email')
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = 'auto_increment_id', 'name', 'description', 'created_date', 'price'
+    list_display = ('auto_increment_id', 'name', 'description', 'created_date', 'price')
 
 
 class DonationAdmin(admin.ModelAdmin):
-    list_display = 'auto_increment_id', 'campaign', 'user', 'created_date', 'price'
+    list_display = ('auto_increment_id', 'campaign', 'user', 'created_date', 'price')
 
 
 class TagAdmin(admin.ModelAdmin):
-    list_display = 'auto_increment_id', 'name', 'slug'
+    list_display = ('auto_increment_id', 'name', 'slug')
 
 
 admin.site.register(Campaign)
-admin.site.register(Profile)
+admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Donation)
 admin.site.register(Tag)
