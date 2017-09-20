908367df9ff8a4a02eb21dea4d0bf94cb38840fb
Raphael Tataia <raphaeltataia@Tataias-Macbook-Pro.local>
2017-09-20 03:56:29 -0300

    including app api urls

--- presentegenial/urls.py
+++ presentegenial/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^app/', include('app.urls')),
+    url(r'^', include('app.api.urls')),
     url(r'^admin/', admin.site.urls),
 ]
