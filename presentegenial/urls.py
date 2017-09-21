ac6f4f91f1b4611c7c64b0ce750088ddd9e156f0
Raphael Tataia <raphaeltataia@Tataias-Macbook-Pro.local>
2017-09-21 15:46:01 -0300

    adding documentation

--- presentegenial/urls.py
+++ presentegenial/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from rest_framework.documentation import include_docs_urls
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('app.api.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^docs/', include_docs_urls(title='Presente Genial')),
+    # url(r'^', include('drf_autodocs.urls')),
 ]
