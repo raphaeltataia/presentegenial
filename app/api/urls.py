630dbb429505127318b44d8161f42fa542f47c66
Raphael Tataia <raphaeltataia@Tataias-Macbook-Pro.local>
2017-09-21 15:45:46 -0300

    adding new urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateProductView, CreateUserView, CreateCampaignView, CreateDonationView, CreateTagView
+from .views import CreateProductView, CreateUserView, CreateCampaignView, DetailCampaignView, CreateDonationView, \
+    CreateTagView, MyCampaignsView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = {
@@ -8,7 +9,9 @@ urlpatterns = {
     url(r'^api/products/$', CreateProductView.as_view(), name="create"),
     url(r'^api/users/$', CreateUserView.as_view(), name="create"),
     url(r'^api/campaigns/$', CreateCampaignView.as_view(), name="create"),
-    url(r'^api/donations/$', CreateDonationView.as_view(), name="create"),
+    url(r'^api/campaigns/my$', MyCampaignsView.as_view(), name="listMyCampaigns"),
+    url(r'^api/campaigns/(?P<id>[0-9]+)$', DetailCampaignView.as_view(), name="CampaignDetail"),
+    url(r'^api/campaigns/(?P<id>[0-9]+)/donate$', CreateDonationView.as_view(), name="CampaignDonate"),
     url(r'^api/tags/$', CreateTagView.as_view(), name="create"),
     url(r'^api/get-token/', obtain_auth_token),
 }
