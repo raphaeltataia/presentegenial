be02ef54e0d160dc9467a1e9cf8eff278fed7277
Raphael Tataia <raphaeltataia@Tataias-Macbook-Pro.local>
2017-09-21 15:44:29 -0300

    adding new permission

--- app/api/permissions.py
+++ app/api/permissions.py
@@ -10,3 +10,14 @@ class IsOwner(BasePermission):
         if isinstance(obj, Campaign):
             return obj.user == request.user
         return obj.user == request.user
+
+
+class IsPostOrIsAuthenticated(BasePermission):
+
+    def has_permission(self, request, view):
+        # allow all POST requests
+        if request.method == 'POST':
+            return True
+
+        # Otherwise, only allow authenticated requests
+        return request.user and request.user.is_authenticated()
